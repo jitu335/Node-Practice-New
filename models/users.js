4a20f226578f8ea5e154000cbe4c35fbf6d7cfae
Jitu <sjitu7271@gmail.com>
2024-06-17 00:07:33 +0530

    27.06.2024 last commit

--- models/users.js
+++ models/users.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true,unique: true },
-}, { timestamps: true });
+}, { timestamps: true });  
 
 
 const userModel = mongoose.model('User', userSchema); 
