8c382d330f683cee0fb57a3b3716dd19509e10c8
Jitu <sjitu7271@gmail.com>
2024-08-14 17:58:06 +0530

    14.08.2024 last commit

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import mongoose from "mongoose";
 import cors from "cors"; 
 import connectToMongo from "./db/config.js"; 
 import router from "./routes/users.js";
-import productsrouter from './routes/products.js'; 
+import productsrouter from './routes/products.js';  
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+const app = express();  
+const PORT = process.env.PORT || 5000;
 
 connectToMongo() 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 app.use(cors()); 
 
 app.use("/api", router);
